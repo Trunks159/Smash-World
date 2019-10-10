691bea8d609beb2b0d8449297e5badf3d6c8d436
Trunks159 <49618679+Trunks159@users.noreply.github.com>
2019-10-10 11:03:43 -0400

    Migrations, ETC
    So many files added

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -17,3 +18,7 @@ class Config():
 
 app = Flask(__name__)
 Config().configure(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+login = LoginManager(app)
+login.login_view = 'login'
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import url_for, render_template
-from config import app
+from flask import url_for, render_template, redirect
+from config import app, db
+from forms import LoginForm
+from models import User
+
 
 @app.route('/')
 @app.route('/index')
@@ -10,9 +13,12 @@ def index():
 def register():
 	return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'Post'])
 def login():
-	return render_template('login.html')
+	form = LoginForm()
+	if form.validate_on_submit():
+		return redirect(url_for('index'))
+	return render_template('login.html', form = form)
 
 @app.route('/tourney')
 def tourney():
